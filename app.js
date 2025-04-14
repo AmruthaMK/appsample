5ffd8372ffb81527e554f546fcf73e27733bb307
studenttest499 <studentstest499@gmail.com>
2025-04-14 18:28:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! hi how are you i am really astonished'));
+app.get('/', (req, res) => res.send('Hello World! hi how are youtonished'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
