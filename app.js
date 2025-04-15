30295f833b22944d15cc0ac93251aedd7257b586
DhanushK2002 <dhanush23702@gmail.com>
2025-04-15 12:03:17 +0530

    New changes from Dhanush

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! hi how are youtonished'));
+app.get('/', (req, res) => res.send('Hello World! This is from St.Philo's'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
